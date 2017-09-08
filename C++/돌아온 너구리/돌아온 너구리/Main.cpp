79bdb81fe2bc0fe642fd6398ad7c6cce07b1a8d3
BakGeon <bak_geon@naver.com>
2017-09-09 05:32:21 +0900

    void DrawMap() 함수 Main.cpp -> Map.h 로 이동

--- C++/돌아온 너구리/돌아온 너구리/Main.cpp
+++ C++/돌아온 너구리/돌아온 너구리/Main.cpp
@@ -3,20 +3,10 @@
 #include "Map.h"
 #include "ConsoleLib.h"
 
-// Define
-#define Lib ConsoleLib::GetInstance()
-
 
 int main(void)
 {
-	for (auto y : map)
-	{
-		for (auto x : y)
-		{
-			Lib->SetColor(x);
-			cout << x;
-		}
+	DrawMap(map);
 
-		cout << endl;
-	}
+	return 0;
 }
--- C++/돌아온 너구리/돌아온 너구리/Map.h
+++ C++/돌아온 너구리/돌아온 너구리/Map.h
@@ -1,5 +1,21 @@
 #pragma once
 #include "stdafx.h"
+#include "ConsoleLib.h"
+
+
+void DrawMap(vector<vector<char>> &map)
+{
+	for (auto y : map)
+	{
+		for (auto x : y)
+		{
+			Lib->SetColor(x);
+			cout << x;
+		}
+
+		cout << endl;
+	}
+}
 
 vector<vector<char>> map
 {
